148e027b6a3d931a4c27a763d9ba65727d230826
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-22 20:15:31 +0100

    Setup Nuxt front-end

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+#__nuxt,
+html,
+body {
+  @apply h-full;
+}
+</style>
---
+++ app/pages/files.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-2xl">Files</div>
+</template>
+
+<script setup lang="ts"></script>
