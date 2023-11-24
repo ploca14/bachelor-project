881cbec9ab35feff7eddec99617ba309f676fe42
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-25 00:15:16 +0100

    Test streaming

--- app/pages/files.vue
+++ app/pages/files.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="text-2xl">Files</div>
+  <div class="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
+    <div class="flex flex-wrap items-center gap-6 pb-8 sm:flex-nowrap">
+      <h1 class="text-3xl font-semibold leading-7 tracking-tight">
+        Study Materials
+      </h1>
+      <UButton
+        icon="i-heroicons-arrow-up-tray-20-solid"
+        size="xl"
+        class="ml-auto"
+        @click="open"
+      >
+        Upload file
+      </UButton>
+    </div>
+
+    {{ progress }}
+
+    <FileGrid />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const progress = ref(0);
+
+// fetch /api/hello and update the progress with the response stream use for await of
+onMounted(async () => {
+  const response = await fetch("/api/hello");
+  if (!response.body) return;
+
+  const reader = response.body.getReader();
+
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) break;
+
+    progress.value = Number(new TextDecoder("utf-8").decode(value));
+  }
+});
+
+const uploader = useFileUploader();
+</script>
--- app/server/api/hello.ts
+++ app/server/api/hello.ts
@@ -2,13 +2,23 @@ import { serverSupabaseClient } from "#supabase/server";
 import type { Database } from "../../types/database";
 
 export default defineEventHandler(async (event) => {
+  // Create a stream that allows writing
+  const { writable, readable } = new TransformStream();
+  sendStream(event, readable);
+  const writer = writable.getWriter();
+
   const client = await serverSupabaseClient<Database>(event);
-  console.log("starting");
+  writer.write("0.25");
+
   await new Promise((resolve) => setTimeout(resolve, 10000));
-  console.log("progress");
+  writer.write("0.5");
   await new Promise((resolve) => setTimeout(resolve, 25000));
-  console.log("done");
+  writer.write("0.75");
+
   await client
     .from("documents")
     .insert({ id: Math.floor(Math.random() * 1000000), content: "test" });
+  writer.write("1");
+
+  writer.close(); // This signals the end of the stream
 });
