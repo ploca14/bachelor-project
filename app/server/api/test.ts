84b887bf5c6bb0a062ea4fa39d577888c81ddd85
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-24 21:38:22 +0100

    Test with supabase

--- app/server/api/test.ts
+++ app/server/api/test.ts
@@ -1,5 +1,16 @@
+import { serverSupabaseClient } from "#supabase/server";
+import type { Database } from "../../types/database";
+
 export default defineEventHandler((event) => {
   setResponseStatus(event, 202);
 
-  $fetch("/api/hello");
+  (async () => {
+    const client = await serverSupabaseClient<Database>(event);
+    console.log("starting");
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    console.log("progress");
+    await new Promise((resolve) => setTimeout(resolve, 25000));
+    console.log("done");
+    client.from("documents").insert({ id: Math.random(), content: "test" });
+  })();
 });
