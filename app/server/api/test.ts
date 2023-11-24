50e2f4609bb33073c08bc8baf338369182570fba
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-24 20:42:37 +0100

    Add time to log

--- app/server/api/test.ts
+++ app/server/api/test.ts
@@ -2,7 +2,7 @@ export default defineEventHandler((event) => {
   setResponseStatus(event, 202);
 
   // Wait 120 seconds before console logging
-  console.log("Waiting 120 seconds...");
+  console.log("Waiting 120 seconds...", new Date());
   setTimeout(() => {
     console.log("Done waiting!");
   }, 120000);
