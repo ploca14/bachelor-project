0e52455a0019b335d233611699a7970e375834e3
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-24 21:00:15 +0100

    Fetch instead of log

--- app/server/api/test.ts
+++ app/server/api/test.ts
@@ -1,14 +1,14 @@
 export default defineEventHandler((event) => {
   setResponseStatus(event, 202);
 
-  console.log("Waiting 10 seconds...", new Date());
+  console.log("Waiting 10 seconds...", new Date().toLocaleDateString());
   setTimeout(() => {
-    console.log("Done waiting!");
+    fetch("/api/hello");
   }, 10000);
 
   // Wait 120 seconds before console logging
-  console.log("Waiting 120 seconds...", new Date());
+  console.log("Waiting 120 seconds...", new Date().toLocaleDateString());
   setTimeout(() => {
-    console.log("Done waiting!");
+    fetch("/api/hello");
   }, 120000);
 });
