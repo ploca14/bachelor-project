82d0706a77a47132cdb880f888be44fc1fbb1d8d
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-24 21:07:16 +0100

    Test async background task by initiating another function using fetch

--- app/server/api/hello.ts
+++ app/server/api/hello.ts
@@ -1,4 +1,10 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
+  console.log("starting");
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  console.log("progress");
+  await new Promise((resolve) => setTimeout(resolve, 120000));
+  console.log("done");
+
   return {
     hello: "world",
   };
--- app/server/api/test.ts
+++ app/server/api/test.ts
@@ -1,14 +1,5 @@
 export default defineEventHandler((event) => {
   setResponseStatus(event, 202);
 
-  console.log("Waiting 10 seconds...", new Date().toLocaleDateString());
-  setTimeout(() => {
-    fetch("/api/hello");
-  }, 10000);
-
-  // Wait 120 seconds before console logging
-  console.log("Waiting 120 seconds...", new Date().toLocaleDateString());
-  setTimeout(() => {
-    fetch("/api/hello");
-  }, 120000);
+  fetch("/api/hello");
 });
