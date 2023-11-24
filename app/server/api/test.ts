80c1449438ad180df75ca74b64d3bbc11ed8a35c
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-24 20:16:26 +0100

    Fixed typo

--- app/server/api/test.ts
+++ app/server/api/test.ts
@@ -4,6 +4,6 @@ export default defineEventHandler((event) => {
   // Wait 120 seconds before console logging
   console.log("Waiting 120 seconds...");
   setTimeout(() => {
-    console.log("Done waiting!";
+    console.log("Done waiting!");
   }, 120000);
 });
