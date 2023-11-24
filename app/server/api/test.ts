e2290af47af58247c6f29efdef785cbf1bd5f398
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-24 20:52:41 +0100

    Test shorter time

--- app/server/api/test.ts
+++ app/server/api/test.ts
@@ -1,6 +1,11 @@
 export default defineEventHandler((event) => {
   setResponseStatus(event, 202);
 
+  console.log("Waiting 10 seconds...", new Date());
+  setTimeout(() => {
+    console.log("Done waiting!");
+  }, 10000);
+
   // Wait 120 seconds before console logging
   console.log("Waiting 120 seconds...", new Date());
   setTimeout(() => {
