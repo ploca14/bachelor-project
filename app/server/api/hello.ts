c676f9ecb362d61487c13825288da6380e0126e2
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-25 01:11:44 +0100

    Encode strings

--- app/server/api/hello.ts
+++ app/server/api/hello.ts
@@ -6,19 +6,20 @@ export default defineEventHandler(async (event) => {
   const { writable, readable } = new TransformStream();
   sendStream(event, readable);
   const writer = writable.getWriter();
+  const encoder = new TextEncoder("utf-8");
 
   const client = await serverSupabaseClient<Database>(event);
-  writer.write("0.25");
+  writer.write(encoder.encode("0.25"));
 
   await new Promise((resolve) => setTimeout(resolve, 10000));
-  writer.write("0.5");
+  writer.write(encoder.encode("0.5"));
   await new Promise((resolve) => setTimeout(resolve, 25000));
-  writer.write("0.75");
+  writer.write(encoder.encode("0.75"));
 
   await client
     .from("documents")
     .insert({ id: Math.floor(Math.random() * 1000000), content: "test" });
-  writer.write("1");
+  writer.write(encoder.encode("1"));
 
   writer.close(); // This signals the end of the stream
 });
