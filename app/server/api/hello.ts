3f8b28dcb8d79409bc53fde26dff394d4426d371
Vojtěch Plocica <plocivoj@fel.cvut.cz>
2023-11-25 01:41:35 +0100

    Readable stream

--- app/server/api/hello.ts
+++ app/server/api/hello.ts
@@ -2,24 +2,31 @@ import { serverSupabaseClient } from "#supabase/server";
 import type { Database } from "../../types/database";
 
 export default defineEventHandler(async (event) => {
-  // Create a stream that allows writing
-  const { writable, readable } = new TransformStream();
-  sendStream(event, readable);
-  const writer = writable.getWriter();
-  const encoder = new TextEncoder("utf-8");
+  const encoder = new TextEncoder();
 
-  const client = await serverSupabaseClient<Database>(event);
-  writer.write(encoder.encode("0.25"));
+  const stream = new ReadableStream({
+    async start(controller) {
+      const write = (chunk: string) => {
+        controller.enqueue(encoder.encode(chunk));
+      };
 
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  writer.write(encoder.encode("0.5"));
-  await new Promise((resolve) => setTimeout(resolve, 25000));
-  writer.write(encoder.encode("0.75"));
+      const client = await serverSupabaseClient<Database>(event);
+      write("0.25");
 
-  await client
-    .from("documents")
-    .insert({ id: Math.floor(Math.random() * 1000000), content: "test" });
-  writer.write(encoder.encode("1"));
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      write("0.5");
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      write("0.75");
 
-  writer.close(); // This signals the end of the stream
+      await client
+        .from("documents")
+        .insert({ id: Math.floor(Math.random() * 1000000), content: "test" });
+
+      write("1");
+
+      controller.close();
+    },
+  });
+
+  return stream;
 });
